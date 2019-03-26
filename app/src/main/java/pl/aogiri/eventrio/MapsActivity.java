4a39f538151d43f5717f8b329849adc46fe90f64
Kaneki #Senpai <v.kaneki.senpai@gmail.com>
2019-03-26 16:55:31 +0100

    repair maps

--- app/src/main/java/pl/aogiri/eventrio/MapsActivity.java
+++ app/src/main/java/pl/aogiri/eventrio/MapsActivity.java
@@ -4,24 +4,16 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
-import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
-import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -37,7 +29,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
@@ -98,6 +89,7 @@ public class MapsActivity extends FragmentActivity implements
 
     private boolean heatmap=false;
 
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,6 +98,8 @@ public class MapsActivity extends FragmentActivity implements
 
         profileContainer = findViewById(R.id.profileContainer);
         imageButton = findViewById(R.id.imageButton);
+        fragment = new EventDetailsFragment();
+
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -120,37 +114,11 @@ public class MapsActivity extends FragmentActivity implements
             }
         });
 
+        service = ServiceGenerator.createService(EventInterface.class, getString(R.string.api_login),getString(R.string.api_password));
 
-        //Create interface for connection
-        service = ServiceGenerator.createService(EventInterface.class, "admin", "f57624a2-fc4c-4d94-8264-c69cf8be676d");
-
-        //Get data from api
-        Call<List<Event>> callEvents = service.listEvents();
-
-        callEvents.enqueue(new Callback<List<Event>>() {
-            @Override
-            public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
-                if (response.isSuccessful() ) {
-                    Log.e(TAG, response.body().toString());
-                    Log.e(TAG,"done");
-                    events = response.body();
-                    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                             .findFragmentById(R.id.map);
                     mapFragment.getMapAsync(MapsActivity.this);
-                } else {
-                    Log.e(TAG, "ops");
-                    Log.e(TAG,response.raw().toString());
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Event>> call, Throwable t) {
-                Log.e(TAG, t.getMessage());
-            }
-        });
-
 
         }
 
@@ -206,61 +174,35 @@ public class MapsActivity extends FragmentActivity implements
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        Log.e(TAG, marker.getTitle());
-        Fragment fragment = new EventDetailsFragment();
         Bundle bundle = new Bundle();
         bundle.putString("id",marker.getTitle());
         fragment.setArguments(bundle);
 //        fragment.setEnterTransition(android.R.transition.slide_bottom);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.setCustomAnimations(R.anim.slide_up, R.anim.slide_down,R.anim.slide_up, R.anim.slide_down);
+        if(fragment.isAdded()){
+            transaction.remove(fragment);
+        }
         transaction
                 .add(R.id.container, fragment)
                 .addToBackStack("fragment")
                 .commit();
-
         return true;
     }
 
-    public void toLocation(LatLng current){
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(current)
-                .zoom(17)
-                .build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-    }
 
 
 
     public void createMark(String title, LatLng position, String image){
         heatmap = false;
-        if(image != null) {
-        byte[] bytes = Base64.decode(image, Base64.DEFAULT);
-        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
 
-
-            mMap.addMarker(
-                    new MarkerOptions()
-                            .title(title)
-                            .position(position)
-                            .icon(BitmapDescriptorFactory.fromBitmap(
-                                    getCircularBitmap((int)(thumRadius*thumbScale),
-                                            ThumbnailUtils.extractThumbnail(Bitmap.createBitmap(bitmap),(int)(thumbHeight*thumbScale), (int)(thumbWidth*thumbScale), ThumbnailUtils.OPTIONS_RECYCLE_INPUT)
-                                    )
-                                )
-                            )
+        mMap.addMarker(
+                new MarkerOptions()
+                           .title(title)
+                           .position(position)
+                           .snippet("This is my stop!")
+                           .icon(BitmapDescriptorFactory.fromBitmap(resize(getDrawable(R.mipmap.placeholder))))
             );
-        }else {
-            mMap.addMarker(
-                    new MarkerOptions()
-                            .title(title)
-                            .position(position)
-                            .snippet("This is my stop!")
-                            .icon(BitmapDescriptorFactory.fromBitmap(resize(getDrawable(R.drawable.default_icon))))
-            );
-        }
-
-        resize(getDrawable(R.drawable.default_icon));
     }
 
 
@@ -290,22 +232,25 @@ public class MapsActivity extends FragmentActivity implements
     }
 
     void fetchData(LatLngBounds bounds){
-        double N = bounds.northeast.latitude;
-        double E = bounds.northeast.longitude;
-        double S = bounds.southwest.latitude;
-        double W = bounds.southwest.longitude;
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.0'Z'");
-        String currentDateandTime = sdf.format(new Date());
-        Call<List<Event>> eventCall = service.listEventsBox(N,E,S,W,currentDateandTime);
+        double E = bounds.northeast.latitude;
+        double W = bounds.southwest.latitude;
+        double S = bounds.northeast.longitude;
+        double N = bounds.southwest.longitude;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentDateAndTime = sdf.format(new Date());
+        Call<List<Event>> eventCall = service.listEventsBox(N,S,W,E, currentDateAndTime);
 
         eventCall.enqueue(new Callback<List<Event>>() {
             @Override
             public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
-                Log.e(TAG,"get response");
-//                mMap.clear();
                 events.clear();
+                Log.e(TAG,response.raw().toString());
+                if(response.code()==204)
+                {
+                    return;
+                }
+
                 double zoom = mMap.getCameraPosition().zoom;
-                Log.e(TAG, String.valueOf(zoom));
                 if(zoom != DEFAULT_ZOOM){
                     thumbScale = zoom / DEFAULT_ZOOM;
                     if(zoom<13){
@@ -313,14 +258,14 @@ public class MapsActivity extends FragmentActivity implements
                         return;
                     }
                 }
+
                 mMap.clear();
                 for(int i = 0 ; i < response.body().size() ; i++){
-
                     Event event = response.body().get(i);
                     String name = event.getId();
 //                    byte[] image = event.getImage();
                     LatLng latLng = new LatLng(event.getLat(),event.getLng());
-                    createMark(name,latLng, event.getImage());
+                    createMark(name,latLng,null);
                     events.add(event);
                 }
             }
@@ -332,23 +277,23 @@ public class MapsActivity extends FragmentActivity implements
             }
         });
     }
-    private Bitmap getCircularBitmap(int radius, Bitmap bitmap) {
-        Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-        Bitmap bmp = Bitmap.createBitmap(radius, radius, conf);
-        Canvas canvas = new Canvas(bmp);
-
-        // creates a centered bitmap of the desired size
-        bitmap = ThumbnailUtils.extractThumbnail(bitmap, radius, radius, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
-        BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-
-        paint.setShader(shader);
-        RectF rect = new RectF(0, 0, radius, radius);
-        canvas.drawRoundRect(rect, radius, radius, paint);
-
-        return bmp;
-    }
+//    private Bitmap getCircularBitmap(int radius, Bitmap bitmap) {
+//        Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+//        Bitmap bmp = Bitmap.createBitmap(radius, radius, conf);
+//        Canvas canvas = new Canvas(bmp);
+//
+//        // creates a centered bitmap of the desired size
+//        bitmap = ThumbnailUtils.extractThumbnail(bitmap, radius, radius, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+//        BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+//        Paint paint = new Paint();
+//        paint.setAntiAlias(true);
+//
+//        paint.setShader(shader);
+//        RectF rect = new RectF(0, 0, radius, radius);
+//        canvas.drawRoundRect(rect, radius, radius, paint);
+//
+//        return bmp;
+//    }
 
     private Bitmap resize(Drawable image) {
         Bitmap b = ((BitmapDrawable)image).getBitmap();
@@ -369,7 +314,7 @@ public class MapsActivity extends FragmentActivity implements
 
         if(!heatmap) mMap.clear();
         heatmap=true;
-        Log.e(TAG, " Hear map");
+
         List<LatLng> list = null;
         list = eventsToLatLngList(date);
 
--- app/src/main/java/pl/aogiri/eventrio/event/EventInterface.java
+++ app/src/main/java/pl/aogiri/eventrio/event/EventInterface.java
@@ -3,8 +3,6 @@ package pl.aogiri.eventrio.event;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -14,13 +12,9 @@ public interface EventInterface {
     @GET("/api/events")
     Call<List<Event>> listEvents();
 
-    @GET("/api/events/box")
-    Call<List<Event>> listEventsBox(@Query("N") double N, @Query("E") double E, @Query("S") double S, @Query("W") double W);
-
     @GET("/api/events/{id}")
     Call<Event> getEvent(@Path("id") String id);
 
-    //TODO test date box
-    @GET("/api/events/box/date")
-    Call<List<Event>> listEventsBox(@Query("N") double N, @Query("E") double E, @Query("S") double S, @Query("W") double W, @Query("date") String date);
+    @GET("/api/events/box")
+    Call<List<Event>> listEventsBox(@Query("N") double N, @Query("S") double S, @Query("W") double W, @Query("E") double E, @Query("date") String date);
 }
